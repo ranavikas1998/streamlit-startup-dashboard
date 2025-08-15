836759a64be055ffc4bcecfdd6c4044defe53a04
Vikas Rana <vikas.developercwc@gmail.comgit  config --global user.email vikas.developercwc@gmail.com>
2025-08-16 00:03:29 +0530

    initial commit

--- app.py
+++ app.py
@@ -10,6 +10,43 @@ st.set_page_config(layout="wide",page_title="Startup Analysis")  # take the spac
 file_path = os.path.join(os.path.dirname(__file__), "data", "startup_cleaned.csv")
 df = pd.read_csv(file_path)
 df["date"] = pd.to_datetime(df["date"], errors="coerce")
+df["month"]=df["date"].dt.month
+df["year"]=df["date"].dt.year
+
+def load_overall_analysis():                # last step find  out month to month sale
+    st.title("Overall analysis")
+    # total invested  amount
+    total=round(df["amount"].sum())
+    # maximum amount infused in a startup
+    max_funding=df.groupby("startup")["amount"].max().sort_values(ascending=False).values[0]
+    # avg ticket size
+    avg_funding=df.groupby("startup")["amount"].sum().mean()
+    # total  funded  startups
+    num_startups=df["startup"].nunique()
+
+    col1,col2,col3,col4=st.columns(4)
+    with col1:
+        st.metric("Total",str(total)+"Cr")
+    with col2:
+        st.metric("Max", str(max_funding) + "Cr")
+    with col3:
+        st.metric("Avg", str(round(avg_funding)) + "Cr")
+    with col4:
+        st.metric("Funded Startups", num_startups)
+
+    st.header("Month on Month(Mom) graph")
+    selected_option=st.selectbox("Select Type",["Total","Count"])
+    if selected_option =="Total":
+        temp_df = df.groupby(["year", "month"])["amount"].sum().reset_index()
+    else:
+        temp_df = df.groupby(["year", "month"])["amount"].count().reset_index()
+    # amount wise funding
+    temp_df["x_axis"] = temp_df["month"].astype("str") + "-" + temp_df["year"].astype("str")
+    fig4, ax4 = plt.subplots()
+    ax4.bar(temp_df["x_axis"], temp_df["amount"].values)
+    st.pyplot(fig4)
+
+
 def load_investor_details(investor):
     st.title(investor)
 
@@ -50,7 +87,6 @@ def load_investor_details(investor):
 
     df["year"] = df["date"].dt.year
     year_series=df[df["Investors"].str.contains("Investors",na=False)].groupby("year")["amount"].sum()
-
     st.subheader("year on Year(YOY) Investments")
     fig3, ax3 = plt.subplots()
     ax3.plot(year_series.index,year_series.values)
@@ -68,8 +104,12 @@ def load_investor_details(investor):
 st.sidebar.title("Startup Funding Analysis")
 option=st.sidebar.selectbox("Select One",["Overall Analysis","Startup","Investor"])
 if option =="Overall Analysis":
-    st.title("Overall Analysis")
-    pass
+     load_overall_analysis()
+#    st.title("Overall Analysis")
+#    btn0=st.sidebar.button("Show Overall Analysis")
+#    if btn0:
+        
+
 elif option =="Startup":
     st.sidebar.selectbox("Select Startup",sorted(df["startup"].unique().tolist())) # convert to the list
     st.title("Startup Analysis")
